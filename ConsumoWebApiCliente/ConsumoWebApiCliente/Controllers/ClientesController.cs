05d32c5a5783498dd626da98ef4e7f51dc5d763c
OLALLA23 <32638142+OLALLA23@users.noreply.github.com>
2018-05-28 09:54:04 +0200

    Initial commit

---
+++ ConsumoWebApiCliente/ConsumoWebApiCliente/Controllers/ClientesController.cs
@@ -0,0 +1,51 @@
+﻿using ConsumoWebApiCliente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ConsumoWebApiCliente.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        ModeloClientes modelo;
+
+        public ClientesController()
+        {
+            this.modelo = new ModeloClientes();
+        }
+        // GET: api/Clientes
+        public IEnumerable<CLIENTES> GetClientes()
+        {
+            return this.modelo.GetClientes();
+        }
+
+        // GET: api/Clientes/5
+        public CLIENTES GetCliente(int id)
+        {
+            return this.modelo.BuscarCliente(id);
+        }
+
+        // POST: api/Clientes
+        //INSERTAR CLIENTE
+        public void Post(CLIENTES cliente)
+        {
+            this.modelo.InsertarCliente(cliente.IDCLIENTE, cliente.NOMBRE,cliente.APELLIDOS, cliente.DIRECCION, cliente.CPOSTAL, cliente.EMAIL);
+        }
+
+        // PUT: api/Clientes/5
+        //MODIFICAR CLIENTE
+        public void Put(int id, CLIENTES cliente)
+        {
+            this.modelo.ModificarCliente(cliente.IDCLIENTE, cliente.NOMBRE, cliente.APELLIDOS, cliente.DIRECCION, cliente.CPOSTAL, cliente.EMAIL);
+        }
+        
+        // DELETE: api/Clientes/5
+        public void Delete(int id)
+        {
+            this.modelo.EliminarCliente(id);
+        }
+    }
+}
---
+++ ConsumoWebApiCliente/ConsumoWebApiCliente/Models/ModeloClientes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumoWebApiCliente.Models
+{
+    public class ModeloClientes
+    {
+        ContextoClientes contexto;
+
+            public ModeloClientes()
+        {
+            this.contexto = new ContextoClientes();
+        }
+
+        public IEnumerable<CLIENTES> GetClientes() //Método para obtener los clientes
+        {
+            var consulta = from datos in contexto.CLIENTES
+                           select datos;
+            return consulta;
+        }
+
+        public CLIENTES BuscarCliente(int idclientes)
+        {
+            CLIENTES clien = contexto.CLIENTES.Find(idclientes);
+            return clien;
+        }
+
+        //METODO POST
+        public void InsertarCliente(int idclientes, String nombre, String apellidos, String direccion, int? codigo, String email)
+        {
+            CLIENTES cliente = new CLIENTES();
+            cliente.IDCLIENTE = idclientes;
+            cliente.NOMBRE = nombre;
+            cliente.APELLIDOS = apellidos;
+            cliente.DIRECCION = direccion;
+            cliente.CPOSTAL = codigo;
+            cliente.EMAIL = email;
+            this.contexto.CLIENTES.Add(cliente);
+            this.contexto.SaveChanges();
+        }
+
+        //METODO PUT
+        public void ModificarCliente(int idclientes, String nombre, String apellidos, String direccion, int? codigo, String email)
+        {
+            CLIENTES cliente = this.BuscarCliente(idclientes);
+            cliente.IDCLIENTE = idclientes;
+            cliente.NOMBRE = nombre;
+            cliente.APELLIDOS = apellidos;
+            cliente.DIRECCION = direccion;
+            cliente.CPOSTAL = codigo;
+            cliente.EMAIL = email;
+            this.contexto.SaveChanges();
+        }
+
+        //METODO DELETE
+        public void EliminarCliente(int idclientes)
+        {
+            CLIENTES cliente = this.BuscarCliente(idclientes);
+            this.contexto.CLIENTES.Remove(cliente);
+            this.contexto.SaveChanges();
+        }
+
+    }
+}
